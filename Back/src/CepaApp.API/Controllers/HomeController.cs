6a7c86d6ff650209418da2a671b1d9ba0dcb0488
Paulo  Castanha <paulo.castanha@kataca.local>
2023-09-08 00:43:26 +0100

    Secçaõ 2: DotNet 5

---
+++ Back/src/CepaApp.API/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CepaApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }  
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Exemplo de get";
+        }
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de post";
+        }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de put = {id}";
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de delete = {id}";
+        }
+    }
+}
